2030a1913fb12e330de0afd9bde2f06e64ad8dc8
Andrew Bays <abays@redhat.com>
2018-03-21 06:59:31 -0400

    Revert "Use abays barometer and aneeshkp collectd"
    This reverts commit ce2166a5609e20d6b3a8701762174949585fc75f.

--- src/package-list.mk
+++ src/package-list.mk
@@ -18,8 +18,8 @@ KAFKA_URL ?= https://github.com/edenhill/librdkafka.git
 KAFKA_TAG ?= v0.9.5
 
 # collectd section
-COLLECTD_URL ?= https://github.com/aneeshkp/collectd
-COLLECTD_TAG ?= collectd-5.8-nested
+COLLECTD_URL ?= https://github.com/redhat-nfvpe/collectd
+COLLECTD_TAG ?= nfvpe/develop
 
 COLLECTD_OPENSTACK_URL ?= https://github.com/openstack/collectd-ceilometer-plugin
 COLLECTD_OPENSTACK_TAG ?= stable/pike
